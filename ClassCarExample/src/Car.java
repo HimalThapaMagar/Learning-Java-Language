17c845fc141e27f3097fa37e60c801e586726afd
himalthapamagar <himalthapamagar@proton.me>
2024-09-27 12:07:23 +0545

    added simplest example of class

---
+++ ClassCarExample/src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    String model = "Challenger";
+    String color = "Blue";
+
+    void Rev()
+    {
+        System.out.println("VRooommmmm VRrooomm");
+    }
+
+    void Drive(){
+        System.out.println("YOu are driving the car.");
+    }
+}
