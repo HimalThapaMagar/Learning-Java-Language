2f4e64d9d2ba359fe7fe2c74e721a4772b9cda5d
himalthapamagar <himalthapamagar@proton.me>
2024-09-29 10:01:08 +0545

    learnt about nested freaky inside and outside class in Java

---
+++ Nested Inside Class Example/src/Outside.java
@@ -0,0 +1,10 @@
+public class Outside {
+    String x = "Hello";
+    public class Inside{
+        String y = "Himal";
+
+        public void SayHello(){
+            System.out.println("Hello, I am Himal Thapa a digital version atleast");
+        }
+    }
+}
