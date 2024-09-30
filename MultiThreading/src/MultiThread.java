01950831a79d6e31262be1bf0a7382c11a7bf163
himalthapamagar <himalthapamagar@proton.me>
2024-09-30 08:40:25 +0545

    learnt about MultiThread in java

---
+++ MultiThreading/src/MultiThread.java
@@ -0,0 +1,23 @@
+// this is one of the way that is well easy to say at the very least than the other one.
+
+//public class MultiThread extends Thread {
+//    public void run(){
+//        System.out.println("A thread is running.");
+//    }
+//    public static void main(String[] args) {
+//        MultiThread mt = new MultiThread();
+//        mt.start();
+//    }
+//}
+
+//Now by another method of creating threads.
+public class MultiThread implements Runnable {
+    public void run(){
+        System.out.println("A new thread is running......");
+    }
+    public static void main(String[] args) {
+        MultiThread mt = new MultiThread();
+        Thread t = new Thread(mt);
+        t.start();
+    }
+}
